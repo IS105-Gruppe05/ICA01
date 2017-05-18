02d3b5b38b99355aacef402aefd2ff9b1c94db6b
Mats Skjaervik <matses15@student.uia.no>
2017-05-18 16:42:21 +0200

    ICA01 helt ferdig

--- Oppgave-1.2.4/Hello.go
+++ Oppgave-1.2.4/Hello.go
@@ -1,17 +1,20 @@
 package main
+
 import "fmt"
+
 func main() {
 	fmt.Println("Hello, 世界")
 	fmt.Println("Mikael var her igjen")
 	fmt.Println("Morten endret dette, wiiiiiiiiiiiiiiiiiiiiiiii")
 	fmt.Println("Hei alle sammen")
 	fmt.Println("Her er enda en endring")
-	fmt.Println("Eirik sier hallo") // KOMMENTAR
+	fmt.Println("Eirik sier hallo")
 	fmt.Println("yolo")
 	fmt.Println("Det var ikke meg")
 	fmt.Println("nils")
 	fmt.Println("Abdi")
 	fmt.Println("Brede")
 	fmt.Println("Jan Kevin #")
+	fmt.Println("Mats endret dette")
 
 }
